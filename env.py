f1dfb8f579f76836e93905c2c464a40341e8999a
Keiji Jin <keijijin@gmail.com>
2023-05-08 15:28:41 +0900

    mask keys

--- env.py
+++ env.py
@@ -2,7 +2,7 @@ import logging
 import os
 
 openaiApiKeyEnv = "OPENAI_API_KEY"
-openaiApiKey = "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"
+openaiApiKey = "XXXXXXXXXXXXXXXXXXXXXXX"
 
 def setenv():
     if openaiApiKeyEnv in os.environ:
